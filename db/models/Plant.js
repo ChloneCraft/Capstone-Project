de01e44612d6af8d44d97a80fa5c2490509ece90
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-28 18:12:00 +0200

    Plant und User Schemas done

---
+++ db/models/Plant.js
@@ -0,0 +1,24 @@
+// db/models/Joke.js
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const plantSchema = new Schema({
+  plantName: { type: String, required: true },
+  type: { type: String, required: true },
+  decayTime: { type: number, required: true },
+  waterCapacity: { type: number, required: true },
+  growthTime: { type: number, required: true },
+  sellers: {
+    type: [{ sellerId: Schema.Types.ObjectId, amount: number, listDate: Date }],
+    required: true,
+  },
+  history: {
+    type: [{ price: number, amount: number, Timestamp: Date }],
+    required: true,
+  },
+});
+
+const Plant = mongoose.models.Plant || mongoose.model("Plant", plantSchema);
+
+export default Plant;
---
+++ db/models/User.js
@@ -0,0 +1,30 @@
+// db/models/Joke.js
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: number, required: true },
+    location: { type: { x: number, y: number }, required: true },
+    totalMoney: { type: number, required: true },
+    currentMoney: { type: number, required: true },
+    plantsCollected: {
+      type: [Schema.Types.ObjectId],
+      ref: "Plant",
+      required: true,
+    },
+    plantStorage: {
+      type: [Schema.Types.ObjectId],
+      ref: "Plant",
+      required: true,
+    },
+    farm: { type: [Schema.Types.ObjectId], ref: "Plant", required: true },
+  },
+  { timestamp: true }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
