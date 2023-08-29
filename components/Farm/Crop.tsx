74738470d95289e8592efa88f89e1698b3d22a54
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-29 18:06:25 +0200

    before adding chakra-ui for image hovering

--- components/Farm/Crop.tsx
+++ components/Farm/Crop.tsx
@@ -1,13 +1,24 @@
 import Image from "next/image";
-import Potato from "../../public/CropStage2.png";
+import Potato from "../../public/PotatoStage3.png";
+import PotatoHover from "../../public/PotatoHover.png";
 
-export default function Crop({ content }: any) {
+export default function Crop({ content }: { content: String }) {
   if (content == "0") {
-    return <></>;
+    return <button className="emptyPlot" />;
   } else {
     return (
       <div className="crop">
-        <Image src={Potato} alt="potato" width={100} height={100} />
+        <Image
+          src={Potato}
+          alt="potato"
+          width={200}
+          height={200}
+          className="cropImage"
+          onMouseOver={(e) => {
+            console.log(e.currentTarget);
+            e.currentTarget.src = "../../public/PotatoHover.png";
+          }}
+        />
       </div>
     );
   }
--- components/Farm/Farm.tsx
+++ components/Farm/Farm.tsx
@@ -1,11 +1,12 @@
 import Crop from "./Crop";
+import { uid } from "uid";
 
 export default function Farm({ farm }: { farm: String[] }) {
   return (
     <section className="farmContainer">
       <div className="farm">
         {farm.map((plot) => {
-          return <Crop content={plot} />;
+          return <Crop content={plot} key={uid()} />;
         })}
       </div>
     </section>
