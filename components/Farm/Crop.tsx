cdf6ba4961f9c5be62a9d490160ce630173054bd
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-30 11:25:20 +0200

    hovering works

--- components/Farm/Crop.tsx
+++ components/Farm/Crop.tsx
@@ -1,27 +1,38 @@
 import Image from "next/image";
 import Potato from "../../public/PotatoStage3.png";
 import PotatoHover from "../../public/PotatoHover.png";
+import { MouseEventHandler } from "react";
+import { useState } from "react";
 
 export default function Crop({ content }: { content: String }) {
+  const [hasMouseOver, setHasMouseOver] = useState(false);
+  const handleMouseEnter: MouseEventHandler<HTMLImageElement> = (e) => {
+    setHasMouseOver(true);
+  };
+  const handleMouseLeave: MouseEventHandler<HTMLImageElement> = (e) => {
+    setHasMouseOver(false);
+  };
   if (content == "0") {
     return <button className="emptyPlot" />;
   } else {
     return (
       <div className="crop">
         <Image
-          src={Potato}
+          src={hasMouseOver ? PotatoHover : Potato}
           alt="potato"
           width={200}
           height={200}
           className="cropImage"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         />
-        <Image
-          src={PotatoHover}
-          alt="potato"
-          width={200}
-          height={200}
-          className="cropImageHover"
-        />
+        {hasMouseOver && (
+          <div
+            className="cropInfo"
+            // onMouseEnter={handleMouseEnter}
+            // onMouseLeave={handleMouseLeave}
+          />
+        )}
       </div>
     );
   }
