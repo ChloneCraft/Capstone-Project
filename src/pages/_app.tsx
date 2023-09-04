5707965e3df94fb0ecfe93ab7ea242d0ed8cd3ca
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-09-04 16:22:29 +0200

    deleteAccount

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import "@/styles/globals.css";
-// import type { AppProps } from "next/app";
+import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps, session }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <SWRConfig
         value={{
-          fetcher: (url) =>
+          fetcher: (url: string) =>
             fetch(url, { next: { revalidate: 2000 } }).then((r) => r.json()),
         }}
       >
