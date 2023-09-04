7144d726d0930336ad0ac8b9655238003bd014d0
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-09-04 15:55:27 +0200

    removing typescript

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,14 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps, session }: AppProps) {
+export default function App({ Component, pageProps, session }) {
   return (
     <SessionProvider session={session}>
       <SWRConfig
         value={{
-          fetcher: (url: string) =>
+          fetcher: (url) =>
             fetch(url, { next: { revalidate: 2000 } }).then((r) => r.json()),
         }}
       >
