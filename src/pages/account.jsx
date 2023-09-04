34eae1381f53e1e8d30a16b498807d12a7d2ea8c
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-09-04 13:39:14 +0200

    authentication on local host works

---
+++ src/pages/account.jsx
@@ -0,0 +1,35 @@
+// import { useSession, signOut, getSession } from "next-auth/react";
+
+// const account = () => {
+//   const { data: session, status } = useSession({ required: true });
+//   if (status === "authenticated") {
+//     return (
+//       <div>
+//         <p>Welcome {session.user.name}</p>
+//         <button onClick={() => signOut()}>sign out</button>
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <div>
+//         <p> you are not signed in</p>
+//       </div>
+//     );
+//   }
+// };
+// export default account;
+
+// // export const getServerSideProps = async (context) => {
+// //   const session = await getSession(context);
+// //   if (!session) {
+// //     return {
+// //       redirect: {
+// //         destination: "/login",
+// //       },
+// //     };
+// //   }
+
+// //   return {
+// //     props: { session },
+// //   };
+// // };
