088c185e747e36482c272b65e844b8d92b3a758b
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-09-07 18:54:00 +0200

    plant images on farm are calculated depending on stage

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -25,29 +25,60 @@ export const authOptions = {
           plantsCollected: [],
           plantStorage: [
             {
+              // potato seed
               plant: "64ef2a7633734c4025ee8e9e",
-              amount: 5,
+              amount: 15,
               decayStatus: 0,
             },
             {
               plant: "64f6e73232e5ea1ee8540236",
-              amount: 5,
+              amount: 15,
               decayStatus: 0,
             },
             {
               plant: "64f8b24fec1cbef6647a818e",
-              amount: 0,
+              amount: 15,
               decayStatus: 0,
             },
             {
               plant: "64f8b201ec1cbef6647a818c",
-              amount: 0,
+              amount: 10,
               decayStatus: 0,
             },
             {
               plant: "64f8b2a0ec1cbef6647a818f",
-              amount: 0,
+              amount: 10,
               decayStatus: 0,
+            },
+            {
+              //maple tree
+              plant: "64f8b2dcec1cbef6647a8190",
+              amount: 10,
+              decayStatus: 150,
+            },
+            {
+              //blueberry
+              plant: "64f8b238ec1cbef6647a818d",
+              amount: 10,
+              decayStatus: 32,
+            },
+            {
+              //pumpkin
+              plant: "64ec84e797da227ff2d81c96",
+              amount: 10,
+              decayStatus: 48,
+            },
+            {
+              //oak tree
+              plant: "64ec850497da227ff2d81c97",
+              amount: 10,
+              decayStatus: 165,
+            },
+            {
+              // potato
+              plant: "64ec81c397da227ff2d81c8d",
+              amount: 10,
+              decayStatus: 70,
             },
           ],
           farm: [
